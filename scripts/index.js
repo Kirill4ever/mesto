b2b5f919f35932cf97f1a379c576507d215be3ac
Kirill Belov <kirillforever@yandex.ru>
2023-04-16 14:08:52 +0300

    pop-ups doing good

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,62 @@
+//Select container and pop-ups
 const container = document.querySelector('.page');
-const popup = container.querySelector('.popup');
-const form = container.querySelector('.popup__form');
+const popupEditProfile = container.querySelector('#popupEditProfile');
+const popupAddCard = container.querySelector('#popupAddCard');
 
+//Select forms
+const editForm = container.querySelector('#editForm');
+const addCardForm = container.querySelector('#addCardForm');
+
+//Select buttons
 const editBtn = container.querySelector('.profile__edit-button');
+const addBtn = container.querySelector('.profile__add-button');
+const closeEditFormBtn = popupEditProfile.querySelector('.popup__close-btn');
+const closeAddFormBtn = popupAddCard.querySelector('.popup__close-btn');
 
+//Select elements for editing
 const authorName = container.querySelector('.profile__author-name');
 const authorDescription = container.querySelector('.profile__author-description');
 
+//Select fields
+const placeNameField = container.querySelector('.popup__text-field_value_place-name');
+const placeImageField = container.querySelector('.popup__text-field_value_place-image');
 const nameField = container.querySelector('.popup__text-field_value_author-name');
 const descriptionField = container.querySelector('.popup__text-field_value_author-description');
 
-function openPopup () {
+//Basic pop-up functions
+function openPopup (popup) {
     popup.classList.add('popup_opened');
+}
+
+function closePopup (popup) {
+    popup.classList.remove('popup_opened');
+}
+
+//Edit profile
+function openEditPopup () {
     nameField.value = authorName.textContent;
     descriptionField.value = authorDescription.textContent;
+    openPopup(popupEditProfile);
 }
+
 function saveChanges (evt) {
     evt.preventDefault();
     let newAuthorName = nameField.value;
     let newAuthorDescription = descriptionField.value;
     authorName.textContent = newAuthorName;
     authorDescription.textContent = newAuthorDescription;
-    closePopup();
+    closePopup(popupEditProfile);
 }
 
-const closeBtn = container.querySelector('.popup__close-btn')
+editBtn.addEventListener('click', openEditPopup);
 
-function closePopup () {
-    popup.classList.remove('popup_opened');
-}
+closeEditFormBtn.addEventListener('click', function() {
+    closePopup(popupEditProfile);
+});
 
-editBtn.addEventListener('click', openPopup);
-closeBtn.addEventListener('click', closePopup);
-form.addEventListener('submit', saveChanges);
+editForm.addEventListener('submit', saveChanges);
 
+//Initial card array
 const initialCards = [
     {
       name: 'Архыз',
@@ -61,27 +84,56 @@ const initialCards = [
     }
   ];
 
-  const cardTemplate = container.querySelector('#card-template');
-  const cardFlex = container.querySelector('.elements__list');
+//Select template and place for cards
+const cardTemplate = container.querySelector('#card-template');
+const cardFlex = container.querySelector('.elements__list');
 
-  function createCardElement(cardData){
-    const cardElement = cardTemplate.content.querySelector('.element').cloneNode(true);
+//Create card element
+function createCardElement(cardData){
+const cardElement = cardTemplate.content.querySelector('.element').cloneNode(true);
 
-    const elementImage = cardElement.querySelector('.element__image');
-    const elementName = cardElement.querySelector('.element__name');
+const elementImage = cardElement.querySelector('.element__image');
+const elementName = cardElement.querySelector('.element__name');
 
-    elementImage.src = cardData.link;
-    elementImage.alt = cardData.name;
-    elementName.textContent = cardData.name;
+elementImage.src = cardData.link;
+elementImage.alt = cardData.name;
+elementName.textContent = cardData.name;
 
-    return cardElement;
-  }
+return cardElement;
+};
 
-  function renderCard(cardElement) {
-    cardFlex.append(cardElement);
-  }
+//Renderer
+function renderCard(cardElement) {
+cardFlex.prepend(cardElement);
+};
 
-  initialCards.forEach((card) => {
-    const element = createCardElement(card);
+//Add initial cards to the page
+initialCards.forEach((card) => {
+const element = createCardElement(card);
+renderCard(element);
+});
+
+//Add new place
+function openAddCardPopup () {
+    placeNameField.value = '';
+    placeImageField.value = '';
+    openPopup(popupAddCard);
+}
+
+function addCard (evt) {
+    evt.preventDefault();
+    let newPlace ={};
+    newPlace.name = placeNameField.value;
+    newPlace.link = placeImageField.value;
+    const element = createCardElement(newPlace);
     renderCard(element);
-  });
+    closePopup(popupAddCard);
+    
+}
+
+addBtn.addEventListener('click', openAddCardPopup);
+closeAddFormBtn.addEventListener('click', function() {
+    closePopup(popupAddCard);
+});
+addCardForm.addEventListener('submit', addCard);
+
